3022dbec4d736326f6eee8a390df76c2d7bd5981
Gary Fung <66684175+gakary@users.noreply.github.com>
2024-07-21 18:20:34 +0800

    updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "mergeTabs") {
+  if (request.action === "mergeTabs") {
     chrome.windows.getAll({populate: true}, function(windows) {
       var tabIds = [];
       for (var i = 0; i < windows.length; i++) {
@@ -8,11 +8,24 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
           tabIds.push(tabs[j].id);
         }
       }
-      chrome.windows.create({tabId: tabIds[0]}, function(newWindow) {
-        for (var k = 1; k < tabIds.length; k++) {
-          chrome.tabs.move(tabIds[k], {windowId: newWindow.id, index: -1});
-        }
-      });
+
+      if (tabIds.length > 0) {
+        chrome.windows.create({tabId: tabIds[0]}, function(newWindow) {
+          if (chrome.runtime.lastError) {
+            console.error('Error creating new window:', chrome.runtime.lastError);
+          } else {
+            for (var k = 1; k < tabIds.length; k++) {
+              chrome.tabs.move(tabIds[k], {windowId: newWindow.id, index: -1}, function() {
+                if (chrome.runtime.lastError) {
+                  console.error('Error moving tab:', chrome.runtime.lastError);
+                }
+              });
+            }
+          }
+        });
+      } else {
+        console.error('No tabs found to merge.');
+      }
     });
   }
 });
